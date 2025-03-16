39f86d64736549217765aec160e753a1e8f074c2
bhargavabhiram1999 <bhargavabhiram1999@gmail.com>
2025-03-17 00:07:05 +0530

    afas

--- src/components/DisplayDashboard.jsx
+++ src/components/DisplayDashboard.jsx
@@ -1,55 +1,66 @@
 import "./dashboard.css";
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 
 export default function DisplayDashboard() {
+    const plainImage = process.env.PUBLIC_URL + "/images/plain.jpg";
+    const xImage = process.env.PUBLIC_URL + "/images/x.jpg";
+    const oImage = process.env.PUBLIC_URL + "/images/o.jpg";
+
     const [player, isPlayer] = useState(0);
-    const [icon, setIcon] = useState(Array(9).fill("/images/plain.jpg"));
+    const [icon, setIcon] = useState(Array(9).fill(plainImage));
     const [winner, setWinner] = useState(null);
 
     function checkWinner() {
-        const checkLiners = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
+        const checkLiners = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6],
+        ];
         for (let [a, b, c] of checkLiners) {
             if (icon[a] === icon[b] && icon[a] === icon[c]) {
-                if(icon[a] === "/images/x.jpg") {
+                if (icon[a] === xImage) {
                     setWinner("X");
-                } else if (icon[a] === "/images/o.jpg") {
-                    setWinner("Y");
+                } else if (icon[a] === oImage) {
+                    setWinner("O");
                 }
             }
         }
     }
 
     function handleClick(index) {
-        
-        console.log("clicked!", index);
         const newIcons = [...icon];
-        
-        if(player%2 ===0) {
-            if(newIcons[index]!=="/images/x.jpg" && newIcons[index]!=="/images/o.jpg") {
-                newIcons[index] = "/images/x.jpg";
-                isPlayer((player)=>(player+1));
-            }            
+
+        if (player % 2 === 0) {
+            if (newIcons[index] !== xImage && newIcons[index] !== oImage) {
+                newIcons[index] = xImage;
+                isPlayer((player) => player + 1);
+            }
         } else {
-            if(newIcons[index]!=="/images/x.jpg" && newIcons[index]!=="/images/o.jpg") {
-                newIcons[index] = "/images/o.jpg";
-                isPlayer((player)=>(player+1));
-            } 
+            if (newIcons[index] !== xImage && newIcons[index] !== oImage) {
+                newIcons[index] = oImage;
+                isPlayer((player) => player + 1);
+            }
         }
         setIcon(newIcons);
-        if(player===8){
+
+        if (player === 8) {
+            alert("It's a draw!");
             window.location.reload();
         }
-        //checkWinner();
     }
 
     useEffect(() => {
         checkWinner();
-        if(winner) {
-            alert(winner);
+        if (winner) {
+            alert(`Winner is: ${winner}`);
             window.location.reload();
         }
-    });
-
+    }, [icon, winner]);
 
     return (
         <div className="center-div">
